ffa62eec6525ef683957910cb7aa4dfaade809a7
nsoumya520-cloud <nsoumya520@gmail.com>
2026-07-06 17:27:31 +0530

    Add files via upload

---
+++ Customer_sql1.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{BC0F3C85-B9E2-4531-823B-8804C630BA21}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {F4BB6717-A582-4E83-B25F-250BAB27B66C}
+	EndGlobalSection
+EndGlobal
